37c65f4befaada8c629a1e70cc90ac3fcda93dec
Seyed Mohammad Sadegh Mahdavi <smahdavi4@gmail.com>
2020-05-17 19:45:48 +0430

    Add name and national id to profiel

--- src/users/forms.py
+++ src/users/forms.py
@@ -19,4 +19,10 @@ class ProfileUpdateForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ['name', 'national_id', 'image']
+
+    def clean_national_id(self):
+        national_id = self.cleaned_data['national_id']        
+        if national_id is not None and (not national_id.isdigit() or len(national_id) != 10):
+            raise forms.ValidationError("National id not valid")
+        return national_id
